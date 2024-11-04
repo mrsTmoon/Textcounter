b0df28122aaac7c55a35ea581b49cb101aacabe8
tasanmudrik <tasan.mudrik@gmail.com>
2024-11-04 14:47:29 +0100

    Changing textcounter

--- TextCounter.java
+++ TextCounter.java
@@ -1,24 +1,20 @@
 package Examination;
 
 public class TextCounter {
+    // Deklarera veriabler som reknar antalet tecken och radar. Deras startvärd är 0
+    private int lineCount = 0;
+    private int characterCount = 0;
 
-    private static int charCount;
-    private static int lineCount;
-
-    public TextCounter() {
-        charCount = 0;
-        lineCount = 0;
-    }
-
-    public static void countLine  (String line){
-        charCount+=line.length();
+    public void addLine(String line) {
         lineCount++;
-
+        characterCount += line.length();
     }
-    public static int getCharCount() {
-        return charCount;
-    }
-    public static int getLineCount() {
+    //Metod som hämtar aktuella värdet från lineCount och retunerar värdet
+    public int getLineCount() {
         return lineCount;
+    }
+    //Metod som hämtar aktuella värdet från characters och retunar värdet
+    public int getCharacterCount() {
+        return characterCount;
     }
 }
